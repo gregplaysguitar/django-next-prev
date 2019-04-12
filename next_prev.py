e5cabe0f974638264b793187a4c8f496c798110b
nkay08 <n.klipp@mailbox.org>
2019-04-12 17:06:51 +0200

    Passes tests

--- next_prev.py
+++ next_prev.py
@@ -53,6 +53,8 @@ def next_or_prev_in_order(instance, qs=None, prev=False, loop=False):
         ordering.append('pk')
         qs = qs.order_by(*ordering)
 
+    # TODO do this recursively ?
+
     for field in ordering:
         if field[0] == '-':
             this_lookup = (lookup == 'gt' and 'lt' or 'gt')
@@ -63,12 +65,75 @@ def next_or_prev_in_order(instance, qs=None, prev=False, loop=False):
                          for f in prev_fields])
         key = "%s__%s" % (field, this_lookup)
         val = get_model_attr(instance, field)
-        if val is not None:
-            q_kwargs[key] = val
-            q_list.append(models.Q(**q_kwargs))
-            prev_fields.append(field)
+        q_kwargs[key] = val
+        q_list.append(models.Q(**q_kwargs))
+        prev_fields.append(field)
+
     try:
-        return qs.filter(reduce(models.Q.__or__, q_list))[0]
+        field = ordering[0]
+        val = get_model_attr(instance, field)
+        qss = qs
+        print("ORDERING", ordering)
+        print("INSTANCE", instance)
+        print("FV", field, val)
+        if val is not None:
+            qss = qs.filter(reduce(models.Q.__or__, q_list))
+        else:
+            qss = qs.filter().exclude(pk=instance.pk)
+            if prev:
+                print("PREV of NULLL")
+                filterterm = {field + "__isnull": True, "pk__lt":instance.pk}
+                qss = qss.filter(**filterterm)
+            else:
+                print("NEXT of NULL")
+                qss = qss.filter(pk__gt=instance.pk)
+
+
+        item = qss[0]
+        print(instance)
+        print("VAL", val, field)
+        print(item, instance)
+        print(get_model_attr(item, field), get_model_attr(instance, field))
+        print(get_model_attr(item, "pk") , get_model_attr(instance, "pk"))
+        print("Yo")
+        print(qss)
+
+        if get_model_attr(item, field) == get_model_attr(instance, field):
+            print("same", item, get_model_attr(item, field), instance, get_model_attr(instance, field))
+            print("QK", q_kwargs)
+            print("QL", q_list)
+            filterterm = {field: val}
+            print("FILTER", filterterm)
+            qss = qss.filter(models.Q(**filterterm))
+            print("new QS", qss)
+            field2 = ordering[1]
+            lookup = "gt"
+            if field2[0] == "-":
+                if prev:
+                    lookup = "gt"
+                else:
+                    lookup = "lt"
+                field2 = field2[1:]
+            else:
+                if prev:
+                    lookup = "lt"
+
+            print("FIELD", field2)
+            filterterm = {}
+            if prev:
+                filterterm = {field2 + "__" + lookup: get_model_attr(instance, field2)}
+                # item = qss.filter(**filterterm).first()
+
+            else:
+                filterterm = {field2 + "__" + lookup: get_model_attr(instance, field2)}
+                # item = qss.filter(**filterterm).first()
+            print("FILTERTERM", filterterm)
+            qss = qss.filter(**filterterm)
+            print(qss)
+            item = qss.first()
+
+        return item
+
     except IndexError:
         length = qs.count()
         if loop and length > 1:
