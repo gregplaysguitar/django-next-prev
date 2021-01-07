45a61086bf23e06c25bb4ecbe7b3c2e233c06fcc
Greg Brown <greg@gregbrown.co.nz>
2021-01-08 09:26:17 +1300

    Remove 2.7 compatibility in tests

--- tests/models.py
+++ tests/models.py
@@ -1,11 +1,6 @@
 from django.db import models
-try:
-    from six import python_2_unicode_compatible
-except ImportError:
-    from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class Category(models.Model):
     title = models.CharField(max_length=100)
 
@@ -13,7 +8,6 @@ class Category(models.Model):
         return self.title
 
 
-@python_2_unicode_compatible
 class Post(models.Model):
     title = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
