83503d6e8f7780def0d5f768bde08105d44eb796
Greg Brown <greg@gregbrown.co.nz>
2021-01-07 20:38:03 +1300

    Django 3.0 compatibility fix

--- tests/models.py
+++ tests/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+try:
+    from six import python_2_unicode_compatible
+except ImportError:
+    from django.utils.encoding import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible
